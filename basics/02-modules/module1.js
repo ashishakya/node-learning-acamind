13cfba0880c1f487f2f54b5c2ca28c36e4a0f218
Ashish Shakya <ashish.shakya@yipl.com.np>
2020-04-02 14:18:44 +0545

    'Initial commit'

---
+++ basics/02-modules/module1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    console.log('Function was called');
+}
+
+let myString = 'This is custom String';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ basics/02-modules/module2.js
@@ -0,0 +1,6 @@
+module.exports = {
+    myFunction: function () {
+        console.log('Exported from module 2');
+    },
+    myVariable: 'Exported variable from module 2'
+};
